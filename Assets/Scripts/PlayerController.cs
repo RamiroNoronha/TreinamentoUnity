4c43e5113082491272978ee3914bad0477e04f0a
RamiroNoronha <100360738+RamiroNoronha@users.noreply.github.com>
2022-03-02 13:21:04 -0300

    Fim do treinamento
    Essa foi a parte final do treinamento de carro, onde apliquei os últimos requisitos, como controle pelo teclado e organização de código

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    //Private variables
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float fowardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // Rotates the car based on vertical Input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
